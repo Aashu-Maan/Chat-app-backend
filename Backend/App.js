dc74b758d5a74ab14e488bcf39f172c3c72b45a3
Aashu-Maan <aashumaan23820@gmail.com>
2025-09-17 19:01:13 +0530

    files updated

--- Backend/App.js
+++ Backend/App.js
@@ -7,7 +7,7 @@ const cookieParser = require("cookie-parser")
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://chat-app-frontend-cj6o.vercel.app",
   credentials: true
 }))
 app.use(cookieParser())
